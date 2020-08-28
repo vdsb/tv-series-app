b951dd19cc8428244cbbf969dd136e9b086c9707
vdsb <68317071+vdsb@users.noreply.github.com>
2020-08-28 12:06:50 +1200

    Updates

---
+++ precache-manifest.6fbc724b00356ae6dd78dd6ad80bd02e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b750e86f0d44b250ef3bdf7468c04d1",
+    "url": "/tv-series-app/index.html"
+  },
+  {
+    "revision": "c64aae901e143298e2b4",
+    "url": "/tv-series-app/static/css/main.c4556be0.chunk.css"
+  },
+  {
+    "revision": "60440201482f0bbb55e5",
+    "url": "/tv-series-app/static/js/2.480e2f36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tv-series-app/static/js/2.480e2f36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c64aae901e143298e2b4",
+    "url": "/tv-series-app/static/js/main.6ed8bebe.chunk.js"
+  },
+  {
+    "revision": "befa017854a7ba8ecd19",
+    "url": "/tv-series-app/static/js/runtime-main.8201dd3d.js"
+  }
+]);
